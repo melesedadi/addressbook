be0749b28a249b6a7d431202e9e59a0b848da253
melesedadi <dadimeles@yahoo.com>
2019-09-23 15:56:07 -0400

    1st commit

---
+++ src/AdressBook.java
@@ -0,0 +1,111 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class AdressBook {
+   private String  name, address, city, state, zip, phoneNumber, email;
+
+    public AdressBook() {
+    }
+
+    public AdressBook(String name, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private boolean isEmailValid(String email){
+        boolean isValid = false;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = email;
+        //Make the comparison case-insensitive.
+        Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if(matcher.matches()){
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber){
+        boolean isValid = false;
+
+        String expression = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
+        CharSequence inputStr = phoneNumber;
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(inputStr);
+        if(matcher.matches()){
+            isValid = true;
+        }
+        return isValid;
+    }
+    public String display(){
+        String display = "Name:" + getName() + "\n" +
+                "address: " + getAddress() +"," + getCity() +"," + getState() +"," + getZip() + "\n" +
+                "Phone Number: " + getPhoneNumber() + "\n" +
+                "Is phone Valid: " +isPhoneNumberValid(phoneNumber)+ "\n" +
+                "Email: " + getEmail() + "\n" +
+                "Is email Valide: " + isEmailValid(email);
+
+        return display;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+/*
+An address book contains the following fields: name, address, city, state, zip, phone, email.
+
+Create an object to hold those fields. Each field should be private and accessible to the main application only through the getters and setters. One advantage of using getters and setters is to allow you to not only set the value of a field, such as email and phone, but also validate the data as it comes in.
+
+Create a setter for phone and email that verify that the phone number is in the following format: (xxx) xxx-xxxx. Create a setter for the email address to ensure it contains name@domain.com.
+
+To validate the email address and phone we're going to use something called regular expressions. We don't need to know about this right now - it's not something we teach until the last few weeks of class. So here is the code to validate these two fields. Simply copy the following methods to your address book class. They are private methods so you will call them from your setter to determine if the email/phone is valid.
+ */
+public class Main {
+
+    public static void main(String [] args){
+        AdressBook adressBook = new AdressBook();
+        adressBook.setName("Nameone");
+        adressBook.setAddress("MC");
+        adressBook.setCity("Silver Spring");
+        adressBook.setState("MD");
+        adressBook.setZip("21390");
+        adressBook.setEmail("metoo@yahoo.com");
+        adressBook.setPhoneNumber("310-546-6890");
+        System.out.println("Information: " +adressBook.display());
+
+
+
+
+    }
+}
